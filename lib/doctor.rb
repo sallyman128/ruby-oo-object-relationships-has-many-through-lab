bcf58113786df7519672c766f194ec208af9131a
Salmaan <salmaan.arif.ali@gmail.com>
2021-01-09 13:47:48 -0500

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,27 @@
+class Doctor
+  attr_reader :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select {|appt| appt.doctor == self}
+  end
+
+  def patients
+    self.appointments.collect {|appt| appt.patient}
+  end
+
+  def self.all
+    @@all
+  end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,26 @@
+class Patient
+  attr_reader :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self}
+  end
+
+  def doctors
+    self.appointments.collect {|appt| appt.doctor}
+  end
+
+  def self.all
+    @@all
+  end
+end
