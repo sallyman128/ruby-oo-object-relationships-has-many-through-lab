dd3fba356eacf61a52f5b044da2af2824f71e2c2
Salmaan <salmaan.arif.ali@gmail.com>
2021-01-09 13:47:44 -0500

    commit tree 348a834aa7cefd3ddb62efca62e66272a0a5f1c0

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+  attr_reader :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    songs.collect {|song| song.artist}
+  end
+
+  def self.all
+    @@all
+  end
+end
